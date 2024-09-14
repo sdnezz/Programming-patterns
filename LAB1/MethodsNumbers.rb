403914879e2d9c29a44e41098f8226b86e22be0f
sdnezz <twinknezz@gmail.com>
2024-09-14 12:06:04 +0300

    4.2 Метод нахождения суммы цифр числа, делящихся на 3 без остатка

--- LAB1/MethodsNumbers.rb
+++ LAB1/MethodsNumbers.rb
@@ -17,11 +17,27 @@ def count_of_coprime_numbers(number)
   	count
 end
 
+#Метод для нахождения суммы цифр числа, делящихся на 3
+def sum_of_digits_div_by_3(number)
+	sum_answer = 0
+	memory_number = 0
+	#Дробление числа на цифры с помощью деления с остатком и переприсвоения делением без остатка на 10
+	while number != 0
+		memory_number = number % 10
+		#Если цифра делится на 3, то считаем сумму
+		if (memory_number %3 == 0)
+			sum_answer = sum_answer + memory_number
+		end
+		number = number / 10
+	end
+	sum_answer
+end
+
+#def divisor_coprime_largest_count_numbers(number)
+#end
+
+#Диалог с пользователем для ввода числа для работы с методами
 puts "Введите число для работы с методами"
 number = gets.chomp.to_i
 puts count_of_coprime_numbers(number)
-
-#def sum_of_digits_div_by_3(number)
-#end
-#def divisor_coprime_largest_count_numbers(number)
-#end
+puts sum_of_digits_div_by_3(number)
