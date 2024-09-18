6684ddc3122939774f1343835c3cdf25b14c765f
sdnezz <twinknezz@gmail.com>
2024-09-18 03:27:03 +0300

    6 - select methods program for aray from file

--- LAB1/ArrayMethods.rb
+++ LAB1/ArrayMethods.rb
@@ -49,12 +49,43 @@ def num_first_pos_el(array)
 	end
 end
 
-#Тестовый список для проверки методов
-array_of_number = [-2,-30,7,2,0,15,12,8,-80]
-#Вывод результатов поиска минимального, максимального и первого положит. эл. соответственно
-minimal_element_in_list = min_element_in_list(array_of_number)
-maximal_element_in_list = max_element_in_list(array_of_number)
-number_first_positive_element_in_list = num_first_pos_el(array_of_number)
-puts "Минимальный элемент списка:", minimal_element_in_list
-puts "Максимальный элемент списка:", maximal_element_in_list
-puts "Номер первого положительного элемента в списке:", number_first_positive_element_in_list
+#Программа, принимающая номер метода и путь к файлу, как два аргумента
+def choise_of_method(num_method,array)
+	if(array.empty?)
+		puts "Массив пуст (проверьте доступ к файлу или целостность содержимого)"
+	else
+		case num_method
+		when 1
+			puts "Минимальный элемент списка: #{min_element_in_list(array)}"
+		when 2
+			puts "Максимальный элемент списка: #{max_element_in_list(array)}"
+		when 3
+			puts "Первый положительный элемент списка: #{num_first_pos_el(array)}"
+		else
+			puts "Выбран несуществующий номер метода"
+		end
+	end
+end
+
+#Выбор пользователем метода и пути к файлу со списком, как аргументов
+if ARGV.length == 2
+	number_of_method = ARGV[0].to_i
+	filepath_of_array = ARGV[1]
+else
+	puts "Введите номер метода для работы со списком:"
+	puts "1) Метод для нахождения минимального элемента списка"
+	puts "2) Метод для нахождения максимального элемента списка"
+	puts "3) Метод для нахождения номера первого положительного элемента в списке"
+	number_of_method = gets.chomp.to_i
+	puts "Введите путь к файлу со списком в формате <название.txt>:"
+	filepath_of_array = gets.chomp
+end
+
+begin
+	#Задаем 2 переменные для просмотра содержимого файла и разбития на массив
+	opened_file = File.read(filepath_of_array)
+	array_from_file = opened_file.split.map{|not_int_array_element| not_int_array_element.to_i }
+
+	#Передаем аргументы и выполняем метод
+	choise_of_method(number_of_method,array_from_file)
+end
