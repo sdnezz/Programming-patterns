51caad4ad367174e584c6a5075c884e547b17ce4
sdnezz <twinknezz@gmail.com>
2024-09-19 02:22:55 +0300

    1.1 - Initialize class Student, getter and setter

---
+++ LAB2/ClassStudent.rb
@@ -0,0 +1,64 @@
+#Соглашения о наименованиях: snake_case, CamelCase, getter, setter=
+class Student
+	#Конструктор класса 
+	def initialize(id, last_name, first_name, middle_name, phone, telegram, email, git)
+		@id = id
+		@last_name = last_name
+		@first_name = first_name
+		@middle_name = middle_name
+		@phone = phone
+		@telegram = telegram
+		@email = email
+		@git = git
+	end
+	#Геттер для каждого поля
+	def id
+		@id
+	end
+	def last_name
+		@last_name
+	end
+	def first_name
+		@first_name
+	end
+	def middle_name
+		@middle_name
+	end
+	def phone
+		@phone
+	end
+	def telegram
+		@telegram
+	end
+	def email
+		@email
+	end
+	def git
+		@git
+	end
+	#Сеттер для каждого поля
+	def id=(new_id)
+		@id = new_id
+	end
+	def last_name=(new_last_name)
+		@last_name = new_last_name
+	end
+	def first_name=(new_first_name)
+		@first_name = new_first_name
+	end
+	def middle_name=(new_middle_name)
+		@middle_name = new_middle_name
+	end
+	def phone=(new_phone)
+		@phone = new_phone
+	end
+	def telegram=(new_telegram)
+		@telegram = new_telegram
+	end
+	def email=(new_email)
+		@email = new_email
+	end
+	def git=(new_git)
+		@git = new_git
+	end
+end
