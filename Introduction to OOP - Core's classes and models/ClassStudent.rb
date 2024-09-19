9b5b91c48465016d12015ebde3b3358d771fc49e
sdnezz <twinknezz@gmail.com>
2024-09-19 15:27:59 +0300

    1.3/1.4 - No duplication with intstance eval, attr atributes

--- Introduction to OOP - Core's classes and models/ClassStudent.rb
+++ Introduction to OOP - Core's classes and models/ClassStudent.rb
@@ -1,66 +1,13 @@
 #Соглашения о наименованиях: snake_case, CamelCase, getter, setter=
 class Student
-	#Конструктор класса 
-	def initialize(id, last_name, first_name, middle_name, phone, telegram, email, git)
-		@id = id
-		@last_name = last_name
-		@first_name = first_name
-		@middle_name = middle_name
-		@phone = phone
-		@telegram = telegram
-		@email = email
-		@git = git
+	#Конструктор класса без дублирования кода с передачей блока для инициализации объекта
+	def initialize(&block)
+		#не прибегая к явному перечислению всех параметров
+		instance_eval &block
 	end
-	#Геттер для каждого поля
-	def id
-		@id
-	end
-	def last_name
-		@last_name
-	end
-	def first_name
-		@first_name
-	end
-	def middle_name
-		@middle_name
-	end
-	def phone
-		@phone
-	end
-	def telegram
-		@telegram
-	end
-	def email
-		@email
-	end
-	def git
-		@git
-	end
-	#Сеттер для каждого поля
-	def id=(new_id)
-		@id = new_id
-	end
-	def last_name=(new_last_name)
-		@last_name = new_last_name
-	end
-	def first_name=(new_first_name)
-		@first_name = new_first_name
-	end
-	def middle_name=(new_middle_name)
-		@middle_name = new_middle_name
-	end
-	def phone=(new_phone)
-		@phone = new_phone
-	end
-	def telegram=(new_telegram)
-		@telegram = new_telegram
-	end
-	def email=(new_email)
-		@email = new_email
-	end
-	def git=(new_git)
-		@git = new_git
-	end
+
+	#Автоматическое создание геттера и сеттера для каждого поля с помощью атрибута
+	attr_accessor :id, :last_name, :first_name, :middle_name, :phone, :telegram, :email, :git
 
 	#Метод класса для отображения текущего состояния объекта
 	def show_object_information
@@ -74,4 +21,3 @@ class Student
 		puts "GitHub: #{@git}"
 	end
 end
-
