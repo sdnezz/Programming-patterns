6f4e1b201a45a1d0772c7866b10ede9ff983ee73
sdnezz <twinknezz@gmail.com>
2024-12-14 04:04:01 +0300

    LR4 (1.1) - Added methods to count rows & columns in matrix
    - добавил методы подсчета строк и столбцов
    - добавил возможность обращаться к элементам по привычным индексам

--- patterns_classes/data_table.rb
+++ patterns_classes/data_table.rb
@@ -5,10 +5,22 @@ class DataTable
   def initialize(matrix)
     self.matrix = matrix
   end
+
   def [](row_index, column_index)
-    self.matrix[row_index][column_index]
+    self.matrix[row_index - 1][column_index - 1]
   end
+  #По каждой row в строку и пробел
   def to_s
     self.matrix.map { |row| row.join(', ') }.join("\n")
   end
+
+  def count_rows
+    #Количество строк = количеству элементов вложенных во внешний массив
+    self.matrix.size
+  end
+  def count_columns
+    #Строка с максимальным количеством элементов - считаем сколько - колонки в матрице
+    self.matrix.max{|a, b| a.size <=> b.size}.size
+  end
+
 end
--- patterns_classes/main_check.rb
+++ patterns_classes/main_check.rb
@@ -1,5 +1,7 @@
 require_relative 'data_table'
 data_table1 = DataTable.new([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-puts data_table1[1,2]
+puts "Элемент 1 строки 2 столбца: #{data_table1[1,2]}"
 puts data_table1
+puts "Количество строк в матрице: #{data_table1.count_rows}"
+puts "Количество столбцов в матрице: #{data_table1.count_columns}"
