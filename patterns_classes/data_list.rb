0b2adec91817fa9c290173a64a562d8b81c81b54
sdnezz <twinknezz@gmail.com>
2024-12-15 21:04:06 +0300

    LR4 (1.7,8) - Sample pattern on get_data & test StudentShort example
    - get data задает базовую реализацию (шаблон), а шаг определения полей переопределяется в наследнике

--- patterns_classes/data_list.rb
+++ patterns_classes/data_list.rb
@@ -1,41 +1,55 @@
 require_relative 'data_table'
 
 class DataList
-	attr_accessor :sorted_array
-	private :sorted_array=
+  attr_accessor :sorted_array
+  private :sorted_array=
 
-	def initialize(array)
-	    self.sorted_array = []
-	    array.sort{|a, b| b <=> a}.each do |element|
-	      self.sorted_array.append({value: element, selected: false})
-	    end
-  	end
-	def [](index)
-		self.sorted_array[index]
-	end
-	def to_s
-		self.sorted_array.to_s
-	end
+  def initialize(array)
+    self.sorted_array = []
+    array.sort { |a, b| b <=> a }.each do |element|
+      self.sorted_array.append({ value: element, selected: false })
+    end
+  end
 
-	def select(index)
-		self.sorted_array[index-1][:selected] = true
-	end
+  def [](index)
+    self.sorted_array[index]
+  end
 
-	def get_selected
-    	self.sorted_array.find_all{|element| element[:selected] == true}
-  	end
+  def to_s
+    self.sorted_array.to_s
+  end
 
-	protected def get_names
-		#Возвращаем массив всех переменных экземпляра
-		obj_fields = self.sorted_array.first[:value].instance_variables
+  def select(index)
+    self.sorted_array[index - 1][:selected] = true
+  end
 
-		# возвращаем без id
-		obj_fields - [:@id]
-  	end
+  def get_selected
+    self.sorted_array.find_all { |element| element[:selected] == true }
+  end
 
-  	# Абстрактный метод get_data
-	protected def get_data
-		raise NotImplementedError, 'Метод get_data должен быть реализован в наследнике'
-	end
+  # Шаблонный метод для get_data
+  def get_data
+    prepare_data(get_names) # Вызываем приватный метод для формирования данных
+  end
 
+  # Методы, которые должны быть реализованы в наследниках
+  protected def get_names
+    raise NotImplementedError, 'Метод get_names должен быть реализован в наследнике'
+  end
+
+  private def prepare_data(column_names)
+    table_data = []
+
+    # Формируем строки таблицы
+    self.sorted_array.each_with_index do |element, index|
+      row = [index + 1] # Нулевой столбец — порядковый номер
+      column_names.each do |name|
+        row << element[:value].instance_variable_get(name)
+      end
+      table_data << row
+    end
+
+    # Создаём объект DataTable
+    DataTable.new(table_data).to_s
+  end
 end
