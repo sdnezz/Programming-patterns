bbe416597ea4b0b53f913ca2855c837ace02f312
sdnezz <twinknezz@gmail.com>
2024-12-27 23:46:06 +0300

    LR4 (2.3) - Created StudentListYAML

--- Student/StudentListYAML.rb
+++ Student/StudentListYAML.rb
@@ -1,35 +1,39 @@
 require_relative 'student'
 require_relative 'student_short'
 require_relative "../patterns_classes/data_list_student_short"
-require 'json'
+require 'yaml'
 
-class StudentsListJSON
+class StudentsListYAML
   attr_accessor :filepath
   attr_reader :student_array
 
   def initialize(filepath:, student_array: nil)
     self.filepath = filepath
-    self.student_array = student_array
+    self.student_array = student_array || []
   end
 
-  private def student_array=(student_array)
-    if student_array.nil? || student_array.is_a?(Array)
+  def student_array=(student_array)
+    if student_array.is_a?(Array) || student_array.nil?
       @student_array = student_array
     else
-      raise TypeError, 'Неверный тип входных данных'
+      raise TypeError, 'Ожидается массив объектов Student или nil'
     end
   end
 
-  private :filepath=
-  
-  # Чтение данных из файла
+  private :filepath=, :student_array=
+
   def read_from_file
-    json_data = JSON.parse(File.read(self.filepath), { symbolize_names: true })
-    student_array_hash = json_data.map { |json_student| Student.new(**json_student) }
+    yaml_data = YAML.load_file(self.filepath, permitted_classes: [Date, Time, Symbol])
+    student_array_hash = yaml_data.map do |yaml_student|
+      # Преобразуем ключи в строки
+      yaml_student[:birthdate] = yaml_student[:birthdate].to_s if yaml_student[:birthdate].is_a?(Date)
+      # Создаём объект Student
+      Student.new(**yaml_student)
+    end
     self.student_array = student_array_hash
   end
 
-  # Запись данных в файл
+  # Запись данных в YAML-файл
   def write_to_file(student_array = self.student_array)
     student_array_hash = student_array.map do |student|
       student.instance_variables
@@ -39,7 +43,7 @@ class StudentsListJSON
     end
 
     File.open(self.filepath, 'w') do |file|
-      file.write(JSON.pretty_generate(student_array_hash))
+      file.write(YAML.dump(student_array_hash))
     end
   end
 
@@ -49,13 +53,10 @@ class StudentsListJSON
     self.student_array.find { |student| student.id == required_id }
   end
 
-  # Получение списка объектов StudentShort (постранично)
-  def get_k_n_student_short_list(page:, amount_rows: 20, data_list: nil)
-    if page <= 0
-      raise ArgumentError, 'Недопустимый номер страницы'
-    elsif amount_rows <= 0
-      raise ArgumentError, 'Недопустимое количество записей'
-    end
+  # Получение списка объектов StudentShort
+  def get_k_n_student_short_list(page:, amount_rows: 20)
+    raise ArgumentError, 'Недопустимый номер страницы' if page <= 0
+    raise ArgumentError, 'Недопустимое количество записей' if amount_rows <= 0
 
     student_short_array = self.student_array.map { |student| StudentShort.new_from_student(student) }
 
@@ -65,20 +66,21 @@ class StudentsListJSON
     DataListStudentShort.new(student_short_array[start_index..end_index])
   end
 
-  # Сортировка по фамилии
+  # Сортировка студентов по фамилии
   def sort_by_name
-    self.student_array.sort_by { |student| student.last_name }
+    self.student_array.sort_by! { |student| student.last_name }
   end
 
-  # Добавление нового студента
+  # Добавление нового студента (с автоформированием ID)
   def add_student(student)
+    max_id = self.student_array.map(&:id).compact.max || 0
+    student.id = max_id + 1
     self.student_array << student
   end
 
   # Замена студента по ID
   def replace_student_by_id(required_id, new_student)
     index = self.student_array.find_index { |student| student.id == required_id }
-
     self.student_array[index] = new_student if index
   end
 
--- Student/main.rb
+++ Student/main.rb
@@ -5,6 +5,8 @@ require_relative 'student_short'
 require_relative 'binary_tree'
 require_relative "../patterns_classes/data_list_student_short"
 require_relative 'StudentListJSON'
+require_relative 'StudentListYAML'
+
 # Создаем несколько экземпляров класса с разными комбинациями необязательных полей
 student_example = Student.new(
 	id: 1,
@@ -112,3 +114,33 @@ students_list.delete_student_by_id(3)
 
 # Сохраняем изменения
 students_list.write_to_file
+
+# Создаём объект
+students_list = StudentsListYAML.new(filepath: 'students.yaml')
+
+# Чтение из файла
+students_list.read_from_file
+
+# Вывод студентов
+puts "Список студентов с yml:"
+students_list.student_array.each { |student| puts student }
+
+# Добавление нового студента
+new_student = Student.new(
+  first_name: "Иван",
+  last_name: "Иванов",
+  middle_name: "Иванович",
+  git: "github.com/ivan",
+  phone: "+79112223344",
+  email: "ivanov@example.com"
+)
+students_list.add_student(new_student)
+
+# Запись в файл
+students_list.write_to_file
+
+# Получение студента по ID
+puts students_list.get_student_by_id(1)
+
+# Получение страницы объектов StudentShort
+puts students_list.get_k_n_student_short_list(page: 1, amount_rows: 2).get_data
